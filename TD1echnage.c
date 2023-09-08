4c2a74698288912d4a0c6ca720498add25186078
David Lou pintrou <78196338+david-alpha@users.noreply.github.com>
2023-09-08 11:42:13 +0200

    Add files via upload

--- TD1echnage.c
+++ TD1echnage.c
@@ -1,15 +1,20 @@
 ALGORITHME Echange
-	DEFINIR
-		entier : a,b,echange
+	DEBUT DEFINIR
+	entier : a,b,echange
 	FIN DEFINIR
 	
 	DEBUT
+		afficher "Donner la valeur de a svp"
 		lire a
+		afficher "Donner la valeur de b svp"
 		lire b
+		afficher "valeur de a avant échange :", a
+		afficher "valeur de b avant échange :", b
 		echange <- a
 		a <- b
 		b <- echange
-		Affiche a,b
+		afficher "valeur de a après échange :", a
+		afficher "valeur de b après échange :", b
 	FIN
 STOP
 	
--- echange.c
+++ echange.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(){
-	//definir
-	int a,b,echange;
-	//fin definir
-	//debut
-		printf("donnez la valeur de a svp : \n");
-		scanf("%d",&a);
-		printf("donnez la valeur de b svp : \n");
-		scanf("%d",&b);
-		echange = a;
-		a = b;
-		b = echange;
-		printf("la valeur de a est %d, la valeur de b est %d ",a,b);
+		//definir
+		int a,b,echange;
+		//fin definir
+
+		//debut
+		printf("donner la valeur de a svp : \n");
+		scanf ("%d",&a);
+		printf("donner la valeur de b svp : \n");
+		scanf ("%d",&b);
+		printf("valeur de a avant echange : %d\n",a);
+		printf("valeur de b avant echange : %d\n",b);
+		echange = a ;
+		a = b ;
+		b = echange ;
+		printf("valeur de a apres echange : %d\n",a);
+		printf("valeur de b apres echange : %d\n",b);
+		//fin
 		return 1;
-	//fin
 }
