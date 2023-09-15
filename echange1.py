6ac25b1bb2c9da255ae7da09bcdd43ed2c77bd17
David Lou pintrou <78196338+david-alpha@users.noreply.github.com>
2023-09-15 10:27:35 +0200

    Add files via upload

---
+++ echange1.py
@@ -0,0 +1,9 @@
+x=int(input("valeur 1 ? ")) # valeur 1 saisie convertie en entier.
+y=int(input("valeur 2 ? ")) # valeur 2 saisie convertie en entier.
+
+echange=x
+x=y
+y=echange
+
+print ("valeur 1 :", x)
+print ("valeur 2 :", y)
